2f8b478909a29774b26235e45d1f790d6ac1c493
Phil <zuschlagp@gmail.com>
2015-10-20 11:38:45 +0200

    Test Klasse.

---
+++ src/application/PNGCreater.java
@@ -0,0 +1,11 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PNGCreater_FX extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	}
+}
