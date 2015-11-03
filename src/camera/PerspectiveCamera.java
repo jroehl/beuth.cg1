39dd65156adbec4212ee776ebe08a75163392769
bodo wissemann <s58880@beuth-hochschule.de>
2015-11-03 17:29:57 +0100

    ray-Methoden implementiert, Kommentare angefangen, .equals(), .hashCode();, .String() generiert in den Kameras und in Ray

--- src/camera/PerspectiveCamera.java
+++ src/camera/PerspectiveCamera.java
@@ -17,11 +17,55 @@ public class PerspectiveCamera extends Camera {
 	@Override
 	public Ray rayFor(int w, int h, int x, int y) {
 
-		final Vector3 v1 = super.w.mul((h / 2) / Math.tan(angle)).mul(-1);
+		final Vector3 v1 = super.w.mul((h / 2) / Math.tan(angle / 2)).mul(-1);
 		final Vector3 v2 = super.u.mul(x - ((w - 1) / 2));
 		final Vector3 v3 = super.v.mul(y - ((h - 1) / 2));
 		final Vector3 r = v3.add(v2).add(v1);
-		final Vector3 d = r.mul(1 / r.magnitude);
-		return new Ray(super.e, d);
+		return new Ray(super.e, r.normalized());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(angle);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final PerspectiveCamera other = (PerspectiveCamera) obj;
+		if (Double.doubleToLongBits(angle) != Double
+				.doubleToLongBits(other.angle))
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "PerspectiveCamera [angle=" + angle + "]";
 	}
 }
--- src/ray/Ray.java
+++ src/ray/Ray.java
@@ -5,22 +5,102 @@ import Matrizen_Vektoren_Bibliothek.Vector3;
 
 public class Ray {
 
-	public final Point3 o;
-	public final Vector3 d;
+	public final Point3 o; // Ursprung
+	public final Vector3 d;// Richtung
 
+	/**
+	 * Der Konstruktor dieser Klasse nimmt als Parameter den Ursprung und die
+	 * Richtung des Strahls entgegen.
+	 *
+	 * @param o
+	 *            repräsentiert den übergebenen Ursprung des Strahls
+	 * @param d
+	 *            repräsentiert die übergebene Richtung des Strahls
+	 */
 	public Ray(Point3 o, Vector3 d) {
 		this.o = o;
 		this.d = d;
 	}
 
+	/**
+	 * Der Strahl hat die Methode at. Diese nimmt als Parameter ein 𝑡 entgegen
+	 * und gibt den entsprechenden Punkt.
+	 *
+	 * @param t
+	 *            repräsentiert den Parameter t, über welchen ein Punkt auf
+	 *            einem vektor definiert wird
+	 * @return
+	 */
 	public Point3 at(double t) {
-		return o;
+		return o.add(d.mul(t));
 
 	}
 
+	/**
+	 * Darüber hinaus hat der Strahl die Methode tOf, welche einen Punkt als
+	 * Parameter annimmt und das entsprechende 𝑡 zurückgibt.
+	 *
+	 * t = (p-o)/d
+	 *
+	 * @param p
+	 *            repräsentiert den übergebenen Punkt p
+	 * @return double t - den Faktor, mit welchem der Vektor multipliziert
+	 *         wurde, um einen bestimmten Punkt zu markieren
+	 */
 	public double tOf(Point3 p) {
-		return 0;
+		return p.sub(o).magnitude / d.magnitude;
 
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((d == null) ? 0 : d.hashCode());
+		result = prime * result + ((o == null) ? 0 : o.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Ray other = (Ray) obj;
+		if (d == null) {
+			if (other.d != null)
+				return false;
+		} else if (!d.equals(other.d))
+			return false;
+		if (o == null) {
+			if (other.o != null)
+				return false;
+		} else if (!o.equals(other.o))
+			return false;
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Ray [o=" + o + ", d=" + d + "]";
 	}
 
 }
