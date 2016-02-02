8f35065eae7576122730cbe4230eb142ad61437a
bodo wissemann <s58880@beuth-hochschule.de>
2016-02-02 14:17:39 +0100

    Begonnen, Sampling zu reparieren

--- src/application/RaytracerOhneGui.java
+++ src/application/RaytracerOhneGui.java
@@ -7,6 +7,7 @@ import geometries.TrianglePyramid;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Set;
 
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
@@ -26,6 +27,7 @@ import javax.imageio.ImageIO;
 import light.Light;
 import light.PointLight;
 import material.LambertMaterial;
+import ray.Ray;
 import ray.Transform;
 import ray.World;
 import textures.SingleColorTexture;
@@ -190,7 +192,7 @@ public class RaytracerOhneGui extends Application {
 		// geometries.add(no10);
 
 		// // Scene 1
-		final Node boxNode = new Node(new Transform().rotateX(0.3).rotateY(0.8).rotateZ(0.4), new ArrayList<Geometry>());
+		final Node boxNode = new Node(new Transform().rotateX(1.5 * Math.PI), new ArrayList<Geometry>());
 		boxNode.geos.add(new TrianglePyramid(new LambertMaterial(new SingleColorTexture(new Color(0.7, 0.7, 0)))));
 		geometries.add(boxNode);
 
@@ -295,9 +297,35 @@ public class RaytracerOhneGui extends Application {
 		for (int y = 0; y < iheight; y++) {
 			for (int x = 0; x < iwidth; x++) {
 
-				final Color c = world.hit(camera.rayFor(iwidth, iheight, x, iheight - 1 - y));
+				// for each ray in camera.rayfor(px, py): Color c =....add()
+				// etc: Durchschnittsfarbe
+				final Set<Ray> raySet = camera.rayFor(iwidth, iheight, x, iheight - 1 - y);
+				final Color retCol = new Color(0, 0, 0);
 
-				final javafx.scene.paint.Color javaColor = new javafx.scene.paint.Color(c.r, c.g, c.b, 1);
+				for (final Ray r : raySet) {
+					retCol.add(world.hit(r));
+				}
+
+				final Color retCol2 = retCol.mul(1.0 / raySet.size());
+				if (retCol2.r > 1) {
+					retCol2.r = 1;
+				} else if (retCol2.r < 0) {
+					retCol2.r = 0;
+				}
+
+				if (retCol2.g > 1) {
+					retCol2.g = 1;
+				} else if (retCol2.g < 0) {
+					retCol2.g = 0;
+				}
+
+				if (retCol2.b > 1) {
+					retCol2.b = 1;
+				} else if (retCol2.b < 0) {
+					retCol2.b = 0;
+				}
+
+				final javafx.scene.paint.Color javaColor = new javafx.scene.paint.Color(retCol2.r, retCol2.g, retCol2.b, 1);
 				wrImg.getPixelWriter().setColor(x, y, javaColor);
 
 			}
--- src/ray/World.java
+++ src/ray/World.java
@@ -4,7 +4,6 @@ import geometries.Geometry;
 import geometries.Hit;
 
 import java.util.ArrayList;
-import java.util.Set;
 
 import light.Light;
 import application.Tracer;
@@ -77,99 +76,31 @@ public class World {
 	 * @return Schnittpunkt mit dem kleinsten positiven t
 	 * @throws IllegalArgumentException
 	 */
-	public Color hit(Set<Ray> rays) throws IllegalArgumentException {
-		Color retCol = new Color(0, 0, 0);
+	public Color hit(Ray ray) throws IllegalArgumentException {
 
-		if (rays == null) {
+		if (ray == null) {
 			throw new IllegalArgumentException("The Ray cannot be null!");
 		}
 
 		Hit hit = null;
+		for (final Geometry obj : objs) {
+			final Hit objHit = obj.hit(ray);
 
-		for (final Ray r : rays) {
-
-			for (final Geometry obj : objs) {
-				final Hit objHit = obj.hit(r);
-
-				if (objHit != null) {
-					if (hit == null || objHit.t < hit.t) {
-						hit = objHit;
-					}
+			if (objHit != null) {
+				if (hit == null || objHit.t < hit.t) {
+					hit = objHit;
 				}
 			}
-
-			if (hit != null) {
-
-				final Color c = hit.geo.material.colorFor(hit, this, new Tracer(this, 5));
-
-				retCol = retCol.add(c);
-				// System.out.println(retCol.r);
-
-			}
-
-			if (hit == null) {
-				retCol = retCol.add(this.backgroundColor);
-			}
 		}
 
-		// nun sorgt nicht mehr der Konstruktor der Color-Klasse dafür, dass
-		// Werte nicht mehr über eins wachsen können, sondern die
-		// Sampling-Pattern-Methode. Jedes Sampling-Pattern muss (!!!!) das
-		// leisten
-
-		final Color retCol2 = retCol.mul(1.0 / rays.size());
-		if (retCol2.r > 1) {
-			retCol2.r = 1;
-		} else if (retCol2.r < 0) {
-			retCol2.r = 0;
+		if (hit != null) {
+			final Color c = hit.geo.material.colorFor(hit, this, new Tracer(this, 5));
+			return c;
 		}
 
-		if (retCol2.g > 1) {
-			retCol2.g = 1;
-		} else if (retCol2.g < 0) {
-			retCol2.g = 0;
-		}
-
-		if (retCol2.b > 1) {
-			retCol2.b = 1;
-		} else if (retCol2.b < 0) {
-			retCol2.b = 0;
-		}
-
-		return retCol2;
-
+		return new Color(0, 0, 0);
 	}
 
-	// public Hit getHit(Ray ray) throws IllegalArgumentException {
-	//
-	// if (ray == null) {
-	// throw new IllegalArgumentException("The Ray cannot be null!");
-	// }
-	//
-	// Hit hit = null;
-	//
-	// final double u = 0.00001;
-	// double t2 = 0;
-	//
-	// for (final Geometry obj : objs) {
-	// hit = obj.hit(ray);
-	//
-	// if (hit != null) {
-	// t2 = hit.t;
-	// }
-	//
-	// if (hit != null && t2 >= u) {
-	// hit.geo.material.colorFor(hit, this, new Tracer(this, 5));
-	// }
-	// }
-	//
-	// if (hit != null) {
-	// return hit;
-	// }
-	//
-	// return null;
-	//
-	// }
 	public Hit getHit(Ray ray) throws IllegalArgumentException {
 
 		if (ray == null) {
